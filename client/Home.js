71df050400818788d9d7899c9ceb3b6bcf8e32fa
Scott Elder <s.m.elder@gmail.com>
2021-04-11 20:16:04 -0500

    fix width

--- client/Home.js
+++ client/Home.js
@@ -2,22 +2,24 @@ import React from "react";
 
 export default function Home() {
   return (
-    <main class="landing">
-      <div class="transparent-layer">
-        <h1 class="announcement">
-          LINDEN
-          <br />
-          AND
-          <br />
-          SCOTT
-          <br />
-          ARE
-          <br />
-          GETTING
-          <br />
-          MARRIED
-        </h1>
-      </div>
-    </main>
+    <div class="full-width-container full-width-container--slanted-border">
+      <main class="landing">
+        <div class="transparent-layer">
+          <h1 class="announcement">
+            LINDEN
+            <br />
+            AND
+            <br />
+            SCOTT
+            <br />
+            ARE
+            <br />
+            GETTING
+            <br />
+            MARRIED
+          </h1>
+        </div>
+      </main>
+    </div>
   );
 }
