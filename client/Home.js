2b2c1650dfd3a6bc95a9a356785fc5ea898b3054
Scott Elder <s.m.elder@gmail.com>
2021-04-12 19:20:32 -0500

    update fonts, styles, and layout

--- client/Home.js
+++ client/Home.js
@@ -1,25 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div class="full-width-container full-width-container--slanted-border">
-      <main class="landing">
-        <div class="transparent-layer">
-          <h1 class="announcement">
-            LINDEN
-            <br />
-            AND
-            <br />
-            SCOTT
-            <br />
-            ARE
-            <br />
-            GETTING
-            <br />
-            MARRIED
-          </h1>
-        </div>
+    <div className="full-width-container full-width-container--slanted-border">
+      <main className="landing">
+        <h1 className="announcement">
+          Linden & Scott
+          <br />
+          Are
+          <br />
+          Getting
+          <br />
+          Married
+        </h1>
       </main>
+      <ul className="topics">
+        <li className="topic">
+          <Link to="/rsvp" className="topic__link">
+            RSVP
+          </Link>
+        </li>
+        <li className="topic">
+          <Link to="/about" className="topic__link">
+            About Us
+          </Link>
+        </li>
+        <li className="topic">
+          <Link to="/zola" className="topic__link">
+            Registry
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
