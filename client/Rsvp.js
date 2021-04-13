afbc5b3f6ad96b875184e3dc2c3d7a0877b6868d
Scott Elder <s.m.elder@gmail.com>
2021-04-12 19:20:09 -0500

    add react-hook-form

--- client/Rsvp.js
+++ client/Rsvp.js
@@ -1,5 +1,31 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 export default function Rsvp() {
-  return <div>RSVP form goes here</div>;
+  const { register } = useForm();
+  return (
+    <form className="rsvp rsvp__form">
+      <header className="rsvp rsvp__header">Respondez s'il vous plait</header>
+      <label className="rsvp rsvp__label">First Name</label>
+      <input className="rsvp rsvp__text-input" name="firstName" />
+
+      <label className="rsvp rsvp__label">Last Name</label>
+      <input className="rsvp rsvp__text-input" name="lastName" />
+
+      <label className="rsvp rsvp__label">Email</label>
+      <input className="rsvp rsvp__text-input" name="email" />
+
+      <label className="rsvp rsvp__label">Phone Number</label>
+      <input className="rsvp rsvp__text-input" name="phone" />
+
+      <label className="rsvp rsvp__label">Current COVID-19 Vaccination Status</label>
+      <input className="rsvp rsvp__text-input" name="currentVaccinationStatus" />
+
+      <label className="rsvp rsvp__label">Expected June 12th COVID-19 Vaccination Status</label>
+      <input className="rsvp rsvp__text-input" name="expectedVaccinationStatus" />
+
+      <label className="rsvp rsvp__label">Any dietary considerations?</label>
+      <input className="rsvp rsvp__text-input" name="dietaryConsiderations" />
+    </form>
+  );
 }
