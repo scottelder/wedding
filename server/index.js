8fc3ef7233ad61e8d371889077a282d6d1ea7303
Scott Elder <s.m.elder@gmail.com>
2021-04-12 20:32:17 -0500

    add boilerplate db -- not working?

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
+require("dotenv").config;
 const express = require("express");
 const port = process.env.PORT || 3030;
 const app = express();
 const path = require("path");
+
+// const db = require("./db");
 
 app.use("/", express.static(path.join(__dirname, "../dist")));
 app.use("/rsvp", express.static(path.join(__dirname, "../dist")));
