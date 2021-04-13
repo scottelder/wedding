64fc0c541d1ba2fc2570c59480f26ed757146a23
Scott Elder <s.m.elder@gmail.com>
2021-04-12 20:04:37 -0500

    set up basic form

--- client/Rsvp.js
+++ client/Rsvp.js
@@ -1,31 +1,34 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 
+const submitCallback = (data) => {
+  console.log(data);
+};
+
 export default function Rsvp() {
-  const { register } = useForm();
+  const { register, handleSubmit } = useForm();
   return (
-    <form className="rsvp rsvp__form">
-      <header className="rsvp rsvp__header">Respondez s'il vous plait</header>
+    <form className="rsvp rsvp__form" onSubmit={handleSubmit(submitCallback)}>
+      <h1 className="rsvp rsvp__header">Respondez s'il vous plait</h1>
       <label className="rsvp rsvp__label">First Name</label>
-      <input className="rsvp rsvp__text-input" name="firstName" />
+      <input className="rsvp rsvp__text-input" {...register("firstName")} />
 
       <label className="rsvp rsvp__label">Last Name</label>
-      <input className="rsvp rsvp__text-input" name="lastName" />
+      <input className="rsvp rsvp__text-input" {...register("lastName")} />
 
-      <label className="rsvp rsvp__label">Email</label>
-      <input className="rsvp rsvp__text-input" name="email" />
-
-      <label className="rsvp rsvp__label">Phone Number</label>
-      <input className="rsvp rsvp__text-input" name="phone" />
+      <label className="rsvp rsvp__label">What is your COVID comfortability?</label>
+      <input className="rsvp rsvp__text-input" {...register("currentVaccinationStatus")} />
 
       <label className="rsvp rsvp__label">Current COVID-19 Vaccination Status</label>
-      <input className="rsvp rsvp__text-input" name="currentVaccinationStatus" />
+      <input className="rsvp rsvp__text-input" {...register("currentVaccinationStatus")} />
 
       <label className="rsvp rsvp__label">Expected June 12th COVID-19 Vaccination Status</label>
-      <input className="rsvp rsvp__text-input" name="expectedVaccinationStatus" />
+      <input className="rsvp rsvp__text-input" {...register("expectedVaccinationStatus")} />
 
       <label className="rsvp rsvp__label">Any dietary considerations?</label>
-      <input className="rsvp rsvp__text-input" name="dietaryConsiderations" />
+      <input className="rsvp rsvp__text-input" {...register("dietaryConsiderations")} />
+
+      <input className="rsvp rsvp__submit" type="submit" />
     </form>
   );
 }
