5a8735068e2aa1be444e430213cabf95f73f29ee
Scott Elder <s.m.elder@gmail.com>
2021-04-13 22:11:18 -0500

    add radio buttons

--- client/Rsvp.js
+++ client/Rsvp.js
@@ -11,6 +11,15 @@ export default function Rsvp() {
     <form className="rsvp rsvp__form" onSubmit={handleSubmit(submitCallback)}>
       <h1 className="rsvp rsvp__header">Respondez s'il vous plait</h1>
 
+      <div className="rsvp rsvp__radio-container">
+        <label className="rsvp rsvp__label rsvp__label--radio">We're coming!</label>
+        <input className="rsvp rsvp__radio-input" type="radio" value="doublet" {...register("rsvp")} />
+        <label className="rsvp rsvp__label rsvp__label--radio">I'm coming!</label>
+        <input className="rsvp rsvp__radio-input" type="radio" value="singlet" {...register("rsvp")} />
+        <label className="rsvp rsvp__label rsvp__label--radio">Cannot attend!</label>
+        <input className="rsvp rsvp__radio-input" type="radio" value="neither" {...register("rsvp")} />
+      </div>
+
       <label className="rsvp rsvp__label">First Name</label>
       <input className="rsvp rsvp__text-input" {...register("firstName")} />
 
