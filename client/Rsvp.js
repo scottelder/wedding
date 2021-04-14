34862b430dbf7d5875f8fc05ec0bf66ffede6baa
Scott Elder <s.m.elder@gmail.com>
2021-04-14 14:25:43 -0500

    change prettier to shorten lines

--- client/Rsvp.js
+++ client/Rsvp.js
@@ -16,10 +16,14 @@ export default function Rsvp() {
       <h1 className="rsvp rsvp__header">Réspondez s'il vous plait</h1>
 
       {errors.rsvp && errors.rsvp.type === "required" && (
-        <span className="rsvp rsvp__error">We need to know if you're coming!</span>
+        <span className="rsvp rsvp__error">
+          We need to know if you're coming!
+        </span>
       )}
       <div className="rsvp rsvp__radio-container">
-        <label className="rsvp rsvp__label rsvp__label--radio">We're coming!</label>
+        <label className="rsvp rsvp__label rsvp__label--radio">
+          We're coming!
+        </label>
         <input
           className="rsvp rsvp__radio-input"
           type="radio"
@@ -27,7 +31,9 @@ export default function Rsvp() {
           {...register("rsvp", { required: true })}
         />
 
-        <label className="rsvp rsvp__label rsvp__label--radio">I'm coming!</label>
+        <label className="rsvp rsvp__label rsvp__label--radio">
+          I'm coming!
+        </label>
         <input
           className="rsvp rsvp__radio-input"
           type="radio"
@@ -35,7 +41,9 @@ export default function Rsvp() {
           {...register("rsvp", { required: true })}
         />
 
-        <label className="rsvp rsvp__label rsvp__label--radio">Cannot attend!</label>
+        <label className="rsvp rsvp__label rsvp__label--radio">
+          Cannot attend!
+        </label>
         <input
           className="rsvp rsvp__radio-input"
           type="radio"
@@ -47,38 +55,75 @@ export default function Rsvp() {
       <section className="rsvp rsvp__section">
         <span className="rsvp rsvp__name-container">
           <label className="rsvp rsvp__label">Individual 1 First Name</label>
-          {errors.individual1 && errors.individual1.firstName && errors.individual1.firstName.type === "required" && (
-            <span className="rsvp rsvp__error rsvp__error--text">Required!</span>
-          )}
-          <input className="rsvp rsvp__text-input" {...register("individual1.firstName", { required: true })} />
+          {errors.individual1 &&
+            errors.individual1.firstName &&
+            errors.individual1.firstName.type === "required" && (
+              <span className="rsvp rsvp__error rsvp__error--text">
+                Required!
+              </span>
+            )}
+          <input
+            className="rsvp rsvp__text-input"
+            {...register("individual1.firstName", { required: true })}
+          />
           <label className="rsvp rsvp__label">Individual 1 Last Name</label>
-          {errors.individual1 && errors.individual1.lastName && errors.individual1.lastName.type === "required" && (
-            <span className="rsvp rsvp__error rsvp__error--text">Required!</span>
-          )}
-          <input className="rsvp rsvp__text-input" {...register("individual1.lastName", { required: true })} />
+          {errors.individual1 &&
+            errors.individual1.lastName &&
+            errors.individual1.lastName.type === "required" && (
+              <span className="rsvp rsvp__error rsvp__error--text">
+                Required!
+              </span>
+            )}
+          <input
+            className="rsvp rsvp__text-input"
+            {...register("individual1.lastName", { required: true })}
+          />
         </span>
         <span className="rsvp rsvp__name-container">
           <label className="rsvp rsvp__label">Individual 2 First Name</label>
-          <input className="rsvp rsvp__text-input" {...register("individual1.firstName")} />
+          <input
+            className="rsvp rsvp__text-input"
+            {...register("individual1.firstName")}
+          />
           <label className="rsvp rsvp__label">Individual 2 Last Name</label>
-          <input className="rsvp rsvp__text-input" {...register("individual1.lastName")} />
+          <input
+            className="rsvp rsvp__text-input"
+            {...register("individual1.lastName")}
+          />
         </span>
       </section>
 
       <label className="rsvp rsvp__label">What is your COVID comfort?</label>
       <input className="rsvp rsvp__text-input" {...register("covidComfort")} />
 
-      <label className="rsvp rsvp__label">Current COVID-19 Vaccination Status</label>
-      <input className="rsvp rsvp__text-input" {...register("currentVaccinationStatus")} />
+      <label className="rsvp rsvp__label">
+        Current COVID-19 Vaccination Status
+      </label>
+      <input
+        className="rsvp rsvp__text-input"
+        {...register("currentVaccinationStatus")}
+      />
 
-      <label className="rsvp rsvp__label">Expected June 12th COVID-19 Vaccination Status</label>
-      <input className="rsvp rsvp__text-input" {...register("expectedVaccinationStatus")} />
+      <label className="rsvp rsvp__label">
+        Expected June 12th COVID-19 Vaccination Status
+      </label>
+      <input
+        className="rsvp rsvp__text-input"
+        {...register("expectedVaccinationStatus")}
+      />
 
       <label className="rsvp rsvp__label">Any dietary considerations?</label>
-      <input className="rsvp rsvp__text-input" {...register("dietaryConsiderations")} />
+      <input
+        className="rsvp rsvp__text-input"
+        {...register("dietaryConsiderations")}
+      />
 
       <label className="rsvp rsvp__label">Anything else we should know?</label>
-      <textarea ref={register} className="rsvp rsvp__textarea" {...register("anythingElse")} />
+      <textarea
+        ref={register}
+        className="rsvp rsvp__textarea"
+        {...register("anythingElse")}
+      />
 
       <input className="rsvp rsvp__submit" type="submit" />
     </form>
